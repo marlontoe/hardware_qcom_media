c2c870969e90087b85c48a0f5d088ce82490fe46
DHO <DHO@dho.im>
2013-02-23 21:00:56 -0500

    Fix Mako build
    include hardware/qcom/display/libgenlock/Android.mk
    include hardware/qcom/display/libqservice/Android.mk
    
    Change-Id: I161b91754354e963e50e8639648b8d3bdb5c9637

--- Android.mk
+++ Android.mk
@@ -1,5 +1,8 @@
 ifneq ($(filter msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 
+include hardware/qcom/display/libgenlock/Android.mk
+include hardware/qcom/display/libqservice/Android.mk
+
 include $(call all-subdir-makefiles)
 
 endif
